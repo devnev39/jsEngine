5a108682e0ef2626bef0fddc57bbe98909739a7a
Dhruv Bonde <devnev39@gmail.com>
2021-06-28 22:36:45 +0530

    in progress

--- collision.js
+++ collision.js
@@ -1,16 +1,26 @@
-function Change(evt, tab) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tab).style.display = "block";
-    evt.currentTarget.className += " active";
+let canvas;
+
+function setup(){
+  canvas = createCanvas(500,200); 
+  canvas.position(50,500);
 }
-  
-  // Get the element with id="defaultOpen" and click on it
+
+function draw(){
+  background('#222222');
+}
+
+function Change(evt, tab) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tab).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+// Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
