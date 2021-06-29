5075fe19a44b61ecd2f3d6bcdadab5fefb765ff3
Dhruv Bonde <devnev39@gmail.com>
2021-06-29 20:51:16 +0530

    matter engine

---
+++ Box_Collision.js
@@ -0,0 +1,63 @@
+function Box(x,y,r){
+    this.r = r;
+    param = {
+        friction : 0,
+        frictionStatic : 0,
+        frictionAir : 0
+    }
+    this.energy = new Energy(this);
+    this.body = Bodies.circle(x,y,r,param);
+    World.add(world,this.body);
+
+    this.show = function(){
+        pos = this.body.position;
+        angle = this.body.angle;
+        this.energy.updateE();
+        push();
+        translate(pos.x,pos.y);
+        stroke(255);
+        fill('white');
+        rotate(angle);
+        rectMode(CENTER);
+        ellipse(0,0,this.r*2);
+        stroke('red');
+        strokeWeight(5);
+        line(0,0,this.r,0)
+        rotate(-angle);
+        //sfill('red');
+        pop();
+    }
+
+    this.changeState = function(vel,angle,restitution,mass,resetAng){
+        this.body.restitution = restitution ? restitution : 0.6;
+        ang = resetAng ? 0 : this.body.angularVelocity;
+        this.body.angle = 0;
+        Body.setMass(this.body,mass);
+        Body.setVelocity(this.body,component(vel,angle));
+        Body.setAngularVelocity(this.body,ang)
+    }
+}
+
+Energy = function(box){
+    this.updateE = function()
+    {
+        //console.log(body.position);
+        let vel = (resultant(box.body.velocity)).toFixed(2);
+        let ang = box.body.angularVelocity;
+        this.KE = +(0.5*box.body.mass*(vel**2)).toFixed(3);
+        this.RKE = +(0.75* box.body.mass* box.r**2 * ang**2).toFixed(3);
+        this.TE = this.KE + this.RKE;
+        return Energy;
+    };
+}
+
+function component(vel,angle){
+    return {
+        x : vel*Math.cos(angle),
+        y : vel*Math.sin(angle)
+    }
+}
+
+function resultant(vel){
+    return Math.sqrt(vel.x**2 + vel.y**2);
+}
--- collision.js
+++ collision.js
@@ -1,3 +1,70 @@
+let canvas;
+var Engine = Matter.Engine,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Body = Matter.Body,
+    Runner = Matter.Runner;
+
+let boxes = [];
+
+function setup(){
+  canvas = createCanvas(500,300);
+  canvas.position(500,500);
+
+  engine = Engine.create();
+  engine.gravity.x = 0;
+  engine.gravity.y = 0;
+  runner = Runner.create();
+  world = engine.world;
+
+  ground = Bodies.rectangle(width/2,height,width+10,30,{isStatic : true});
+  up = Bodies.rectangle(width/2,0,width+10,30,{isStatic : true}); 
+  right = Bodies.rectangle(width,height/2,30,height+10,{isStatic : true}); 
+  left = Bodies.rectangle(0,height/2,30,height+10,{isStatic : true}); 
+  ground.restitution = up.restitution=right.restitution=left.restitution = 1;
+  for(let i=0;i<20;i++){
+    boxes.push(new Box(getRandom(width),getRandom(height),5));
+  }
+
+  boxes.forEach(box=>{
+    box.changeState(10,radians(getRandom(360)),1,1,false);
+  });
+  
+  World.add(world,[ground,up,left,right]);
+  World.add(world,boxes);
+  Runner.run(runner,engine);
+
+  setInterval(deltaPos,500);
+  
+}
+
+function draw(){
+  background('black');
+  boxes.forEach(box=>{
+    box.show();
+  })
+  fill(170);
+  stroke('white');
+  rectMode(CENTER);
+  rect(width/2,height,width,30);
+  rect(width/2,0,width,30);
+  rect(width,height/2,30,height);
+  rect(0,height/2,30,height);
+}
+
+function deltaPos(){
+  let te = 0;
+  boxes.forEach(box=>{
+    te+=box.energy.TE;
+  })
+  console.log(te);
+  console.log(boxes[0].body.angularVelocity);
+}
+
+function getRandom(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function Change(evt, tab, color) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
